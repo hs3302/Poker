fa6ca446ced5c55b1e4e75543346994fdc7e0417
hs3302 <120994270+hs3302@users.noreply.github.com>
2023-06-24 17:08:53 -0400

    split to 9 sub-lists

--- Class & Unsorted List.py
+++ Class & Unsorted List.py
@@ -4,6 +4,7 @@
 # In[ ]:
 
 from itertools import combinations
+from Identification import *
 
 # Define a Card class including suit and number:
 class Card:
@@ -27,3 +28,34 @@ for suit in Card.suits:
 
 # Full unsorted list of 5 cards from the deck:
 unsorted_list_5 = list(combinations(Deck,5))
+
+# Split the full unsorted list into 9 sub-lists
+SF_unsorted = []
+FK_unsorted = []
+FH_unsorted = []
+Fl_unsorted = []
+St_unsorted = []
+TK_unsorted = []
+TP_unsorted = []
+OP_unsorted = []
+HC_unsorted = []
+
+for i in unsorted_list_5:
+    if IdSF(i) == True:
+        SF_unsorted.append(i)
+    elif IdFK(i) == True:
+        FK_unsorted.append(i)
+    elif IdFH(i) == True:
+        FH_unsorted.append(i)
+    elif IdFl(i) == True:
+        Fl_unsorted.append(i)
+    elif IdSt(i) == True:
+        St_unsorted.append(i)
+    elif IdTK(i) == True:
+        TK_unsorted.append(i)
+    elif IdTP(i) == True:
+        TP_unsorted.append(i)
+    elif IdOP(i) == True:
+        OP_unsorted.append(i)
+    else:
+        HC_unsorted.append(i)
